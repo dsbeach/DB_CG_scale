8c3bdd183902a9ea0490cda6cfa73d69541094f1
David Beach <david.s.beach@gmail.com>
2018-03-16 15:15:04 -0400

    Removed .01 mm settings from options.

--- option_handler.cpp
+++ option_handler.cpp
@@ -32,36 +32,26 @@ int OptionHandler::handle(int button, I2C_LCD lcd, HX711 *frontCell, HX711 *rear
       break;
     case 1:
       lcd.printrow(0, "Span Dist. 1's");
-      lcd.printrow(1, String(eepromValues->spanDistance,2));
+      lcd.printrow(1, String(eepromValues->spanDistance,1));
       changeValue(button, &eepromValues->spanDistance, 1.0);
       break;
     case 2:
       lcd.printrow(0, "Span Dist. .1's");
-      lcd.printrow(1, String(eepromValues->spanDistance,2));
+      lcd.printrow(1, String(eepromValues->spanDistance,1));
       changeValue(button, &eepromValues->spanDistance, 0.1);
       break;
     case 3:
-      lcd.printrow(0, "Span Dist. .01's");
-      lcd.printrow(1, String(eepromValues->spanDistance,2));
-      changeValue(button, &eepromValues->spanDistance, 0.01);
-      break;
-    case 4:
       lcd.printrow(0, "Peg Dist. 1's");
-      lcd.printrow(1, String(eepromValues->pegDistance,2));
+      lcd.printrow(1, String(eepromValues->pegDistance,1));
       changeValue(button, &eepromValues->pegDistance, 1.0);
       break;
-    case 5:
+    case 4:
       lcd.printrow(0, "Peg Dist. .1's");
-      lcd.printrow(1, String(eepromValues->pegDistance,2));
+      lcd.printrow(1, String(eepromValues->pegDistance,1));
       changeValue(button, &eepromValues->pegDistance, 0.1);
       break;
-    case 6:
-      lcd.printrow(0, "Peg Dist. .01's");
-      lcd.printrow(1, String(eepromValues->pegDistance,2));
-      changeValue(button, &eepromValues->pegDistance, 0.01);
-      break;
     // set units of measure
-    case 7:
+    case 5:
       lcd.printrow(0, "Btn 1 g Btn 2 oz");
       lcd.printrow(1, "Btn 3 = both");
       switch (button)
